2dcb56804545b664c7dfcfaebc1a2976b9202d46
Hassan Qasemi <hassanak813@outlook.com>
2025-12-22 03:10:44 +0330

    batman

---
+++ app/app.config.ts
@@ -0,0 +1,16 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: "orange",
+      neutral: "zinc",
+    },
+  },
+  header: {
+    title: "NUCH",
+    showTitle: true,
+    logo: {
+      light: "/logo.png",
+      dark: "/logo.png",
+    },
+  },
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  extends: ["@nuxtjs/plausible"],
+  modules: ["@nuxt/ui"],
+  css: ["~/assets/css/main.css"],
+});
