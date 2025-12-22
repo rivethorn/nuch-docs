2ac4f2b609b874571a77cfbc5120cd12a39e6d4d
Hassan Qasemi <hassanak813@outlook.com>
2025-12-22 03:40:04 +0330

    header logo

---
+++ app/components/AppHeaderLogo.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
+
+<template>
+    <div class="inline-flex items-center gap-1.5">
+        <UColorModeImage v-if="appConfig.header?.logo?.dark || appConfig.header?.logo?.light"
+            :light="appConfig.header?.logo?.light || appConfig.header?.logo?.dark"
+            :dark="appConfig.header?.logo?.dark || appConfig.header?.logo?.light"
+            :alt="appConfig.header?.logo?.alt || appConfig.header?.title" class="h-6 w-auto shrink-0" />
+        <span>
+            {{ appConfig.header?.title || '{appConfig.header.title}' }}
+        </span>
+    </div>
+</template>
